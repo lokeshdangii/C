94e93bd526729ece2cf381da9e15332cabb6a609
lokeshdangii <lokeshdangi1045@gmail.com>
2023-03-05 12:56:32 +0530

    addition

---
+++ Addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+printf("------Program for addition-------");
+
+int a,b,c;
+
+printf("\n Enter the first value --");
+scanf("%d", &a);
+
+printf("\n Enter the second value --");
+scanf("%d", &b);
+
+c = a + b;
+
+printf("\n Addition of two values is -- %d", c);
+
+return 0;
+}
